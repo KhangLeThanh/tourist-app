6dc50b154dc7818388de6db5dc84266a3aaf3dd2
Khang Le Thanh <khangthanh2312@gmail.com>
2025-10-24 13:36:42 +0300

    creating api for accomodations view

--- app/screens/AccommodationsScreen.tsx
+++ app/screens/AccommodationsScreen.tsx
@@ -1,10 +1,100 @@
-import React from "react";
-import { Text, View } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { MaterialIcons } from "@expo/vector-icons";
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { fetchPlaces, Place } from "../api/geoapify";
+import { fetchLocationResult } from "../api/geoLocation";
+import { useSearch } from "../context/SearchContext";
+import { useDebounce } from "../hooks/useDebounce";
+import { globalStyles } from "../styles/globalStyles";
 
 const AccommodationsScreen: React.FC = () => {
+  const [accomodations, setAccomodations] = useState<Place[]>([]);
+  const [loading, setLoading] = useState(true);
+  const {
+    location,
+    suggestions,
+    setSuggestions,
+    selectedPlace,
+    setSelectedPlace,
+  } = useSearch();
+  const debouncedQuery = useDebounce(location, 600);
+
+  useEffect(() => {
+    const fetchLocation = async () => {
+      if (!debouncedQuery.trim() || debouncedQuery.length < 2) {
+        setSuggestions([]);
+        return;
+      }
+      const results = await fetchLocationResult(debouncedQuery);
+      setSuggestions(results);
+    };
+    fetchLocation();
+  }, [debouncedQuery]);
+  useEffect(() => {
+    const loadRestaurances = async () => {
+      setLoading(true);
+
+      if (selectedPlace) {
+        const data = await fetchPlaces(
+          selectedPlace.lat,
+          selectedPlace.lon,
+          "accommodation.hotel"
+        );
+        setAccomodations(data);
+      }
+      setLoading(false);
+    };
+    loadRestaurances();
+  }, [selectedPlace]);
+
   return (
-    <View>
-      <Text> This is a new view</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.title}>Find accomodations</Text>
+      <SearchBar />
+
+      {suggestions.length > 0 && (
+        <View style={globalStyles.dropdown}>
+          <FlatList
+            data={suggestions}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                style={globalStyles.suggestion}
+                onPress={() => {
+                  setSelectedPlace(item);
+                  setSuggestions([]);
+                }}
+              >
+                <Text>{item.formatted}</Text>
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+      )}
+      {loading ? (
+        <ActivityIndicator size="large" style={{ flex: 1 }} />
+      ) : (
+        <FlatList
+          data={accomodations}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View style={globalStyles.card}>
+              <View style={globalStyles.cardHeader}>
+                <Text style={globalStyles.name}>{item.name}</Text>
+                <MaterialIcons name="bed" size={20} color="#f57c00" />
+              </View>
+              <Text style={globalStyles.address}>{item.address_line2}</Text>
+            </View>
+          )}
+        />
+      )}
     </View>
   );
 };
